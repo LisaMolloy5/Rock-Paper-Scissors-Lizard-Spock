d9ef5640e1003f676fbe9f116b7ba481860b5906
LisaMolloy5 <molloylisa5@gmail.com>
2022-04-30 14:12:52 +0000

    Create variables for game

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 import random
 
+### Welcome Message 
+
 print("-------------------------------------------")
 print("Welcome to Rock Paper Scissors Lizard Spock")
 print("-------------------------------------------")
